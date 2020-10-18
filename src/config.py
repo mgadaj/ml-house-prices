cccf7cddeeb1621ab3f262c58a3e7ccae7a4d75d
Maciej Gadaj <gadmacpg@gmail.com>
2020-10-18 23:22:35 +0200

    config.py file added.

---
+++ src/config.py
@@ -0,0 +1,27 @@
+# data
+DATA_FILE =
+TARGET =
+
+# pipeline
+PIPELINE_NAME =
+
+# variables
+FEATURES =
+
+# variables to drop
+DROP_FEATURES =
+
+# numerical variables with nan
+NUMERICAL_VAR_WITH_NAN =
+
+# categorical variables with nan
+CATEGORICAL_VAR_WITH_NAN =
+
+# temporal variables
+TEMPORAL_VAR =
+
+# variables to log transform
+NUMERICAL_VAR_LOG =
+
+# categorical variables to encode
+CATEGORICAL_VAR_ENCODE =
